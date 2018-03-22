ceb3752f1cb3dc006b1f358a2a3c387396611b3e
jiro.kirameki <ec2-user@ip-172-31-41-240.us-east-2.compute.internal>
2018-03-22 12:52:26 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   root to: 'toppages#index'
   
   get 'signup', to: 'users#new'
+  post 'login', to: 'session#create'
+  delete 'logout', to: 'session#destroy'
+  
+  get 'signup', to: 'users#new'
   resources :users, only: [:show, :new, :create]
 end
