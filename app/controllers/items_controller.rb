10704fa64840e3759d0309fefc7609da1ad7c451
jiro.kirameki <ec2-user@ip-172-31-41-240.us-east-2.compute.internal>
2018-03-23 14:20:49 +0000

    want

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,38 +1,27 @@
 class ItemsController < ApplicationController
   before_action :require_user_logged_in
   
+  def show
+    @item = Item.find(params[:id])
+    @want_users = @item.want_users
+  end
+  
   def new
     @items = []
     
     @keyword = params[:keyword]
-    if @keyword.present? #
-      results = RakutenWebService::Ichiba::Item.search([
+    if @keyword
+      results = RakutenWebService::Ichiba::Item.search({
         keyword: @keyword,
         imageFlag: 1,
         hits: 20,
-      ])
+      })
       
       results.each do |result|
         # 扱い易いように　Item としてインスタンスを作成する　（保存はしない）
-        item = Item.new(read(result))
+        item = Item.find_or_initialize_by(read(result))
         @items << item
       end
     end
-  end
-  
-  private
-  
-  def read(result)
-    code = result['itemCode']
-    name = result['itemName']
-    url = result['itemUrl']
-    image_url = result['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128×128', '')
-    
-    return {
-      code: code,
-      name: name,
-      url: url,
-      image_url: image_url,
-    }
   end
 end
