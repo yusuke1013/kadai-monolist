5c6cd7dd718342ff3b492afb07c415ad87895d02
jiro.kirameki <ec2-user@ip-172-31-41-240.us-east-2.compute.internal>
2018-03-26 01:52:37 +0000

    ranking have

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   #resources :ownershiphaves, only: [:create, :destroy]
   
   get 'rankings/want', to: 'rankings#want'
+  get 'rankings/have', to: 'rankings#have'
 end
